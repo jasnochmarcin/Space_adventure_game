fdc7bfb3c5d497cb1db5816786b3f11a09d80fae
jasnochmarcin <74145933+jasnochmarcin@users.noreply.github.com>
2021-05-17 11:29:22 +0200

    Update ship.py
    Add update method and update __init__

--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship:
     """A class designed to manage a spacecraft."""
 
@@ -9,12 +10,23 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        """Loading a spacecraft image and retrieving its rectangle"""
+        # Loading a spacecraft image and retrieving its rectangle
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        """Each new spacecraft appears at the bottom of the screen"""
+        # Each new spacecraft appears at the bottom of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Options indicating the movement of the spacecraft
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Updating the position of a spacecraft based on an option indicating its movement."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Display the spacecraft in its current position."""
