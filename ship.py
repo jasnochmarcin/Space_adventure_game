d1a099f9292758f1f06d1879d303be2c8c3edc90
jasnochmarcin <74145933+jasnochmarcin@users.noreply.github.com>
2021-05-18 17:01:09 +0200

    Update ship.py

--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
         """Initialization of the spacecraft and its initial position."""
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Loading a spacecraft image and retrieving its rectangle
@@ -17,16 +18,23 @@ class Ship:
         # Each new spacecraft appears at the bottom of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # The ship position is stored as a float
+        self.x = float(self.rect.x)
+
         # Options indicating the movement of the spacecraft
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Updating the position of a spacecraft based on an option indicating its movement."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # Updating the x-coordinate value of a ship, not its rectangle
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Updating a rect object based on the value of self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Display the spacecraft in its current position."""
