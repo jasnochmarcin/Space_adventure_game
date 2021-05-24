da7bb0196932f5ef8220e284effc35a4dbb8a933
jasnochmarcin <74145933+jasnochmarcin@users.noreply.github.com>
2021-05-24 15:15:41 +0200

    Update ship.py

--- ship.py
+++ ship.py
@@ -39,3 +39,7 @@ class Ship:
     def blitme(self):
         """Display the spacecraft in its current position."""
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
