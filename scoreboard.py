b60f55e7495f86a0d49623ec1d214747cc0c0f69
jasnochmarcin <74145933+jasnochmarcin@users.noreply.github.com>
2021-05-27 17:55:20 +0200

    Added high scores and lives system

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,15 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
+
 
 class Scoreboard:
     """A class designed to store score information."""
 
     def __init__(self, ai_game):
         """Initialization of scoring attributes."""
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -16,10 +21,13 @@ class Scoreboard:
 
         # Prepare initial images with score
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         """Converting a score into a generated image."""
-        rounded_score =round(self.stats.score, -1)
+        rounded_score = round(self.stats.score, -1)
         score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
@@ -31,3 +39,41 @@ class Scoreboard:
     def show_score(self):
         """Displaying the scores on the screen."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
+
+    def prep_high_score(self):
+        """Converting the best score in the game to the generated image."""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+        # Display the best score in the game in the top middle of the screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.screen_rect.top
+
+    def check_high_score(self):
+        """Checking to see if we have a new best score in the game."""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+
+    def prep_level(self):
+        """Converting the level number to the generated image."""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+        # Level number is displayed below the current score.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        """Displays the number of ships the player has left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
