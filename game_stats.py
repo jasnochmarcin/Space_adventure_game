4516f05c2831235d36105549e9de112fa938ea75
jasnochmarcin <74145933+jasnochmarcin@users.noreply.github.com>
2021-05-27 17:55:46 +0200

    Added high scores and lives system

--- game_stats.py
+++ game_stats.py
@@ -9,7 +9,11 @@ class GameStats:
         # Start Space Adventur in an active state.
         self.game_active = False
 
+        # The best score should be retained
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialize statistics that may change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
